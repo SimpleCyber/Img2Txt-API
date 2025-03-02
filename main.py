dbebd607f9333a2f282dd7ae1724b91c4c0995b5
Satyam Yadav <satyamok03@gmail.com>
2025-03-02 11:55:36 +0530

    #old one easy ocr

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-import pytesseract
-from PIL import Image
 from flask import Flask, request, jsonify
 import cv2
+import easyocr
 import numpy as np
 
 app = Flask(__name__)
+
+# Define local model path
+local_model_path = "./model"
+
+# Initialize EasyOCR Reader using local model files
+reader = easyocr.Reader(
+    ['en'],  # English language
+    gpu=False,  # Use CPU (set True if using GPU)
+    model_storage_directory=local_model_path  # Prevents downloading, uses local models
+)
+
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({"message": "Hello", "SimpleCyber": "Img2Txt-API"})
 
 @app.route('/extract_text', methods=['POST'])
 def extract_text():
@@ -14,13 +27,11 @@ def extract_text():
     file = request.files['image']
     image_np = np.frombuffer(file.read(), np.uint8)
     img = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
-
-    # Convert OpenCV image to PIL format
-    pil_img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-
-    # Extract text using Tesseract
-    extracted_text = pytesseract.image_to_string(pil_img)
-
+    
+    # Extract text
+    text_results = reader.readtext(img)
+    extracted_text = [text for _, text, _ in text_results]
+    
     return jsonify({"extracted_text": extracted_text})
 
 if __name__ == '__main__':
