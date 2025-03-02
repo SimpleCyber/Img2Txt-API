36ca63b7ab48be2a46b754388beda55ee3f36be3
Satyam Yadav <satyamok03@gmail.com>
2025-03-02 11:49:14 +0530

    # build file

---
+++ render-build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install system dependencies
+apt-get update && apt-get install -y \
+    tesseract-ocr \
+    libtesseract-dev \
+    libleptonica-dev
+
+# Install Python dependencies
+pip install -r requirements.txt
